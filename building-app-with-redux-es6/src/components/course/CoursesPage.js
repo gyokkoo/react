7309d22b8b0d45b6b5eb01e3ce53dbaab93f0a9c
Gyokan <gyokan.nezhdetov@gmail.com>
2017-08-16 23:40:26 +0300

    Added actions, reducers, store

--- building-app-with-redux-es6/src/components/course/CoursesPage.js
+++ building-app-with-redux-es6/src/components/course/CoursesPage.js
@@ -1,13 +1,52 @@
 import React, { PropTypes } from 'react';
+import { connect } from 'react-redux';
+import * as courseActions from '../../actions/courseActions';
 
 class CoursesPage extends React.Component {
+
+  constructor (props, context) {
+    super(props, context);
+
+    this.state = {
+      course: {title: ''}
+    };
+
+    this.onTitleChange = this.onTitleChange.bind(this);
+    this.onClickSave = this.onClickSave.bind(this);
+  }
+
+  onTitleChange (event) {
+    const course = this.state.course;
+    course.title = event.target.value;
+    this.setState({course: course});
+  }
+
+  onClickSave () {
+    this.props.dispatch(courseActions.createCourse(this.state.course));
+  }
+
   render () {
     return (
       <div>
         <h1>Courses</h1>
+        <h2>Add Course</h2>
+        <input
+          type="text"
+          onChange={this.onTitleChange}
+          value={this.state.course.title}/>
+        <input
+          type="submit"
+          value="Save"
+          onClick={this.onClickSave}/>
       </div>
     );
   }
 }
 
-export default CoursesPage;
+function mapStateToProps (state, ownProps) {
+  return {
+    courses: state.courses
+  }
+}
+
+export default connect(mapStateToProps)(CoursesPage);
